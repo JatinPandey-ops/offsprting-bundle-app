987dc4236d2242ac7b844cdaa05885721ccf4b80
Jatin Pandey <pandeyjatin7788@gmail.com>
2024-09-12 08:50:21 +0530

    db.server

--- app/db.server.js
+++ app/db.server.js
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-// Check if globalThis already has a Prisma instance, else initialize it
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-// In development, store Prisma instance in globalThis to avoid multiple instances
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
+// Make sure Prisma is only instantiated once in a server environment
+let prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
