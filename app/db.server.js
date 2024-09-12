80d680d471780aa20097affb0b13c3819031e2c6
Jatin Pandey <pandeyjatin7788@gmail.com>
2024-09-12 09:03:10 +0530

    db.server prsima error

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = global.prisma || new PrismaClient();
+// Function to create a new PrismaClient instance
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+// Check if globalThis already has a Prisma instance, otherwise create one
+const prisma = globalThis.prismaGlobal || prismaClientSingleton();
+
+// In development, store the Prisma instance in globalThis to prevent multiple instances
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
 }
 
 export default prisma;
