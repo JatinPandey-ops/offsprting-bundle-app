b03990d66757a752e671f39fcd12d41997f8575a
Jatin Pandey <pandeyjatin7788@gmail.com>
2024-12-02 07:16:34 +0530

    changed inventory

--- prisma/migrations/20240918181746_add_bundle_variant_relation/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `quantity` on the `BundleVariant` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "BundleVariant" DROP COLUMN "quantity";
