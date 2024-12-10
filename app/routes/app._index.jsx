66ff298e82ec51db7aa48e6cf667ff46e3ba98ea
Jatin Pandey <pandeyjatin7788@gmail.com>
2024-12-10 06:53:50 +0530

    ui update 7

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -231,7 +231,7 @@ export default function BundlesPage() {
   const formatPrice = useCallback((price) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'MYR',
     }).format(price || 0);
   }, []);
 
